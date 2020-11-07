9fb582e3cf4d47ab7bdc029a873c612ada9500b3
MohamedHiggy <mohamed.higgy31@gmail.com>
2020-11-07 05:44:44 +0200

    first commit

---
+++ Starter/components/HomeCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <img :src="home.images[0]" width="200px"> <br/>
+      {{home.title}} <br/>
+      {{home.location.address}} <br/>
+      {{home.location.city}} <br/>
+      {{home.location.state}} <br/>
+      {{home.guests}} / guests <br/>
+      {{home.pricePerNight}} / Night <br/>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      home: {
+        type: Object,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ Starter/pages/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="container">
+    <div v-for="home in homes" :key="home.objectID" class="card">
+      <nuxt-link :to="`/home/${home.objectID}`" no-prefetch><HomeCard :home="home"></HomeCard></nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import homes from "@/data/homes";
+export default {
+  head() {
+    return {
+      title: "Homepage",
+      meta: [{
+        name: "description",
+        content: "this is a homepage!",
+        hid: "description"
+      }]
+    }
+  },
+  data() {
+    return {
+      homes: homes.slice(0,4)
+    }
+  },
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  justify-content: center;
+  text-align: center;
+  display: flex;
+  flex-wrap: wrap;
+}
+.card {
+  border: 1px solid red;
+  padding: 15px;
+  width: 33.333%;
+  margin: 0 auto;
+}
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+</style>
